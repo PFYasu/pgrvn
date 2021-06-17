b334c576460a653b3139a65649bd9b1096d3c392
Omer Lagziel <l.omer@ravendb.net>
2021-06-17 12:43:36 +0300

    RavenDB-16880 Perform actions on PostgreSQL DB using Npgsql

--- pgrvn/pgrvn/Program.cs
+++ pgrvn/pgrvn/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace pgrvn
 {
@@ -6,7 +9,27 @@ namespace pgrvn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connString = "Host=127.0.0.1;Username=postgres;Password=123456;Database=BookStore";
+
+            using var conn = new NpgsqlConnection(connString);
+            conn.Open();
+
+            // Insert some data
+            using (var cmd = new NpgsqlCommand("INSERT INTO \"Customers\" (\"First Name\") VALUES (@p)", conn))
+            {
+                cmd.Parameters.AddWithValue("p", NpgsqlDbType.Name, "Hello world");
+                cmd.ExecuteNonQuery();
+            }
+
+            // Retrieve all rows
+            using (var cmd = new NpgsqlCommand("SELECT \"First Name\" FROM \"Customers\"", conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+            }
         }
     }
 }
